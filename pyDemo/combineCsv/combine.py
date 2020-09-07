2cdea66826f67e51dff87a8bad72d13b4ff4e39b
shr26207 <sunhaoran@offcn.com>
2020-09-07 14:13:15 +0800

    plan B

--- pyDemo/combineCsv/combine.py
+++ pyDemo/combineCsv/combine.py
@@ -1,12 +1,16 @@
 import time
 import os
-inputPath = "E:/myPractice/result.csv/"
+
 outputFile = "sub_" + time.strftime("%Y-%m-%d-%H-%M", time.localtime()) + ".csv"
 fpOut = open(outputFile, "w")
 fpOut.writelines(["WindNumber,Time,label\n"])
-dirList = os.listdir(inputPath)
-for file in dirList:
-    if file.endswith(".csv"):
-        fpIn = open(inputPath+file, "r")
-        fpOut.writelines(fpIn.readlines())
-        print(file)
+
+for n in range(1, 13):
+    inputPath = "E:/myPractice/result_%s" % n + "/"
+    dirList = os.listdir(inputPath)
+    print(inputPath)
+    for file in dirList:
+        if file.endswith(".csv"):
+            fpIn = open(inputPath+file, "r")
+            fpOut.writelines(fpIn.readlines())
+            print(file)
