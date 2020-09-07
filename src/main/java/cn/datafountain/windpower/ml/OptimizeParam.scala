3034e940ac16f9d7fd567981de0603b956d53b30
shr26207 <sunhaoran@offcn.com>
2020-09-07 11:30:48 +0800

    plan B

--- src/main/java/cn/datafountain/windpower/ml/OptimizeParam.scala
+++ src/main/java/cn/datafountain/windpower/ml/OptimizeParam.scala
@@ -21,48 +21,27 @@ case class WindParams2(
                         RotorSpeed: Double,
                         spType: Int
                       )
-object OptimizeParam extends App with  Context{
+object OptimizeParam {
 
-  val trainData: DataFrame = loadData()
+  def OptimizeK(trainData:DataFrame,start:Int,end:Int,step:Int): Unit ={
+    val featuresArray = Array("WindSpeed","Power","RotorSpeed")
+    val vecDF = new VectorAssembler().setInputCols(featuresArray).setOutputCol("features")
 
+    //规范化
+    val scalaDF = new StandardScaler().setInputCol("features").setOutputCol("scaledFeatures")
+      .setWithStd(true).setWithMean(false)
+    //Pipeline 组装
+    //var kMeans = new KMeans().setFeaturesCol("scaledFeatures").setK(70).setSeed(123456789)
 
-  //类别型字段转换
-  //private val encoder1: OneHotEncoder = new OneHotEncoder().setInputCol("spType").setOutputCol("spTypeVector").setDropLast(false)
-
-  //组合特征向量
-  val featuresArray = Array("WindSpeed","Power","RotorSpeed")
-  val vecDF = new VectorAssembler().setInputCols(featuresArray).setOutputCol("features")
-
-  //规范化
-  val scalaDF = new StandardScaler().setInputCol("features").setOutputCol("scaledFeatures")
-    .setWithStd(true).setWithMean(false)
-  //Pipeline 组装
-  //var kMeans = new KMeans().setFeaturesCol("scaledFeatures").setK(70).setSeed(123456789)
-
-  private val pipeline: Pipeline = new Pipeline().setStages(Array(vecDF, scalaDF))
-  private val data2: DataFrame = pipeline.fit(trainData).transform(trainData)
-  /*val kMeans = new BisectingKMeans().setFeaturesCol("scaledFeatures").setK(48).setSeed(123456789)
-  val model = kMeans.fit(data2)
-  val WSSSE: Double = model.computeCost(data2)
-  println(s"Within  Set  Sum  of  Squared  Errors=  $WSSSE")*/
-
-  val KSSE = (30 to 60 by 2).toList.map{
-    k =>
-      val biKMeans = new BisectingKMeans().setFeaturesCol("scaledFeatures").setK(k).setSeed(123456789)
-      val model = biKMeans.fit(data2)
-      val WSSSE: Double = model.computeCost(data2)
-      /*val centers = model.clusterCenters
-      val d = Vectors.sqdist(centers(0), centers(1))
-      println()
-      println(d)*/
-      (k,WSSSE)
+    val pipeline: Pipeline = new Pipeline().setStages(Array(vecDF, scalaDF))
+    val data2: DataFrame = pipeline.fit(trainData).transform(trainData)
+    val KSSE = (start to end by step).toList.map{
+      k =>
+        val biKMeans = new BisectingKMeans().setFeaturesCol("scaledFeatures").setK(k).setSeed(123456789)
+        val model = biKMeans.fit(data2)
+        val WSSSE: Double = model.computeCost(data2)
+        (k,WSSSE)
+    }
+    KSSE.sortBy(_._1).foreach(println)
   }
-  KSSE.sortBy(_._1).foreach(println)
-
-  // K select 48
-
-
-  // TODO 1 Top size  cluster 最大簇 中心
-  // TODO 2 sqdist/size 每个簇到最大簇中心的距离平方比本簇大小 -> 系数平均值 avg
-  // TODO 2 系数< avg*(0.4) 标记为 1 异常数
 }
--- src/main/java/cn/datafountain/windpower/ml/WindPowerPlanB.scala
+++ src/main/java/cn/datafountain/windpower/ml/WindPowerPlanB.scala
@@ -1,7 +1,15 @@
 package cn.datafountain.windpower.ml
 
-import cn.datafountain.windpower.common.Context
+import breeze.numerics.log10
+import cn.datafountain.windpower.common.{Context, WindResult, WindResultOut}
+import cn.datafountain.windpower.ml.WindPowerApp.{sparkSession, trainData}
+import org.apache.spark.ml.{Pipeline, linalg}
+import org.apache.spark.ml.clustering.BisectingKMeans
+import org.apache.spark.ml.feature.{StandardScaler, VectorAssembler}
+import org.apache.spark.ml.linalg.Vectors
 import org.apache.spark.sql.DataFrame
+
+import scala.collection.mutable
 
 object WindPowerPlanB extends App with Context{
   val trainData: DataFrame = loadData()
@@ -9,7 +17,61 @@ object WindPowerPlanB extends App with Context{
       .distinct().orderBy("WindNumber").collect()
     rows.foreach(x=> {
       val n = x(0)
-      val value = trainData.filter(s"WindNumber=$n")
-      value.show(1)
+      val trainDataOne = trainData.filter(s"WindNumber=$n").toDF()
+      println(s"WindNumber:$n")
+
+      val featuresArray = Array("WindSpeed","Power","RotorSpeed")
+      val vecDF = new VectorAssembler().setInputCols(featuresArray).setOutputCol("features")
+      val k = 10
+      //规范化
+      val scalaDF = new StandardScaler().setInputCol("features").setOutputCol("scaledFeatures")
+        .setWithStd(true).setWithMean(false)
+      //Pipeline 组装
+      val pipeline: Pipeline = new Pipeline().setStages(Array(vecDF, scalaDF))
+      val data2: DataFrame = pipeline.fit(trainDataOne).transform(trainDataOne)
+      val biKMeans = new BisectingKMeans().setFeaturesCol("scaledFeatures").setK(k).setSeed(123456789)
+      val model = biKMeans.fit(data2)
+      val clusterSizes: Array[Long] = model.summary.clusterSizes
+      val centers: Array[linalg.Vector] = model.clusterCenters
+      var maxSize: Long = 0L;
+      var maxSizeCluster = 0;
+      for (i <- clusterSizes.indices
+           if clusterSizes(i)>maxSize
+           ) {
+        maxSize = clusterSizes(i)
+        maxSizeCluster = i
+      }
+      println("maxSizeCluster="+maxSizeCluster)
+      var clusterDistFactor = new Array[Double](k)
+      for (i <- clusterSizes.indices
+           if i != maxSizeCluster
+           ) {
+        val sqDist = Vectors.sqdist(centers(maxSizeCluster),centers(i))
+        clusterDistFactor(i) = log10(clusterSizes(i)/sqDist)
+
+        println(s"i=$i;size="+clusterSizes(i)+";dist:"+sqDist+";Factor:"+clusterDistFactor(i))
+      }
+      val avgFactor = clusterDistFactor.sum/clusterDistFactor.length;
+      println("maxSizeCluster avgFactor ="+avgFactor)
+      val errorProportion = 1; // 错误比例1->0.29532939814  1.2->0.70278337  1.3->0.69500249943   1.5 -> 0.66424782047
+      val errorCluster:mutable.Set[Int] = mutable.Set()
+      for (cluster <- clusterDistFactor.indices
+           if cluster != maxSizeCluster
+           if clusterDistFactor(cluster)< avgFactor*errorProportion
+           ) {
+        errorCluster.add(cluster)
+        println(s"Error Cluster：$cluster ;Factor:"+clusterDistFactor(cluster))
+      }
+      val resultDF: DataFrame = model.transform(data2)
+      import sparkSession.implicits._
+      resultDF.select("WindNumber","Time",model.getPredictionCol)
+        .as[WindResult]
+        .map( result => {
+          val label = if (errorCluster.contains(result.prediction)) 1 else 0;
+          WindResultOut(result.WindNumber,result.Time,label)
+        })
+        .write
+        .mode("append")
+        .csv("result.csv")
     })
 }
